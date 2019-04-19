08c79b360cdd1b857a0250eff5cf2dfce3974f19
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-19 15:37:29 +1200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,8 @@
 module.exports = async client => {
   // Log that the bot is online.
+  console.log('I am ready!');
+  console.log('Version 2');
+  console.log('Bot: Hosting ' + `${client.users.size}` + ' users, in ' + `${client.channels.size}` + ' channels of ' + `${client.guilds.size}` + ' guilds.');
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 
   // Make the bot "play the game" which is the help command with default prefix.
