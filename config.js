a9abea8e4f6d5052e1912ef675bcf93f9e69793c
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-23 11:23:39 +1200

    Updated to be like downloaded repo

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ const config = {
 
   // Your Bot's Token. Available on https://discordapp.com/developers/applications/me
   "token": "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0",
+  //"token": "actual token here",
 
   // Default per-server settings. New guilds have these settings. 
 
@@ -23,9 +24,10 @@ const config = {
     "adminRole": "Verdancy",
     "systemNotice": "true", // This gives a notice when a user tries to run a command that they do not have permission to use.
     "welcomeChannel": "welcome-list",
-    "welcomeMessage": "Say hello to {{user}}, everyone! We all need a warm welcome sometimes :D",
+    "welcomeMessage": "Say hello to {{user}}, everyone! We all need a warm welcome sometimes :'D",
     "welcomeEnabled": "false"
   },
+
 
   // PERMISSION LEVEL DEFINITIONS.
 
