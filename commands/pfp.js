70417932f1b00ceffbb81cc7a7112fa3f99d3223
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-27 20:56:00 +1200

    Til fixed everything. I hope.

--- commands/pfp.js
+++ commands/pfp.js
@@ -1,11 +1,28 @@
 const { RichEmbed } = require('discord.js');
 
-exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+exports.run = async (client, message, args, level, r, unbClient) => {
+    var url;
 
-  if (args.length > 0) {
-    if 
-    
+    if (args.length > 0) {
+        if (message.mentions.members.first()) {
+            url = message.mentions.members.first().user.avatarURL;
+        } else {
+            message.channel.send()("There is no mention");
+            return;
+        }
+    } else {
+        url = message.author.avatarURL;
+    }
+
+    sendEmbed(message.channel, url);
 };
+
+async function sendEmbed(channel, url) {
+    var embed = new RichEmbed()
+            .setImage(url)
+            .setColor('#68AD36')
+    let msg = await channel.send({ embed });
+}
 
   //const let pfp = message.author.displayAvatarURL
   //const msg = await message.channel.send("Test", {files: [message.author.displayAvatarURL]});
