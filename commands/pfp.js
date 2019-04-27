86d9e4bf793fcce0e9adc9c5a7736b4e3db6c156
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-27 18:55:17 +1200

    Update and rename count.js to pfp.js

---
+++ commands/pfp.js
@@ -0,0 +1,18 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const let pfp = message.author.displayAvatarURL
+  const msg = await message.channel.send(pfp);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "pfp",
+  category: "Miscelaneous",
+  description: "It takes a photo of ya and then gives it out",
+  usage: "pfp"
+};
