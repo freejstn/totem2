efe017c9f3ecafb03b2facd441fd72a284b2cf51
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-27 20:08:42 +1200

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -3,7 +3,7 @@ const { RichEmbed } = require('discord.js');
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
 
   //if (message.content === -1) {
-  const url = message.mentions.members.first();
+  const url = message.author.avatarURL;
   //.indexOf("{")
   var embed = new RichEmbed()
             .setImage(url)
