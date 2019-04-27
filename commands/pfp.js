72ca410817dbfc76f58f878606755159b01e3f78
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-27 19:37:01 +1200

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -1,11 +1,13 @@
 
 const { RichEmbed } = require('discord.js');
+const url = message.author.avatarURL;
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   //const let pfp = message.author.displayAvatarURL
   //const msg = await message.channel.send("Test", {files: [message.author.displayAvatarURL]});
   var embed = new RichEmbed()
-            .setImage(message.author.displayAvatarURL)
+            .setImage(url)
+            .setColor(colour)
         let msg = await channel.send({ embed });
 };
 //Looking good ;)
