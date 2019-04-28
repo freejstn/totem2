f446bbd18a91dcc7ffc01f9921f5a457178cf1ec
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-28 17:24:15 +1200

    Update topic.js

--- commands/topic.js
+++ commands/topic.js
@@ -1,11 +1,9 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   //const msg = await message.channel.send("coming soon lol");
   let replies = ["sample text 1", "sample text 2", "sample text 3"];
-
-
   let result = Math.floor((Math.random() * replies.length));
 
-  let question = args.slice(1).join(" ");
+//  let question = args.slice(1).join(" ");
 
 
   message.channel.send(replies[result]);
