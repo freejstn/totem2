92d67a6836836f21dac4c00d10cc96d5128d744d
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-28 17:42:02 +1200

    added some topics

--- commands/topic.js
+++ commands/topic.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   //const msg = await message.channel.send("coming soon lol");
   
-  let replies = ["sample text 1", "sample text 2", "sample text 3"];
+  let replies = ["What are the magic numbers on your parents credit card?", "On a scale of 1-10, how much of a weeb are you?", "Where did you hide the bodies?", "On a scale of `The best` to `Better than the best` how much of a great supreme is Kiwi?", "How do I hide multiple Orange coprses?", "That Malice kid is pretty trash, amight?", "Who's Grass?"];
   let result = Math.floor((Math.random() * replies.length));
 
   //  let question = args.slice(1).join(" ");
