28c2d2b905ad126ba107ce5d6277618129d9b760
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-28 17:46:05 +1200

    Update topic.js

--- commands/topic.js
+++ commands/topic.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   //const msg = await message.channel.send("coming soon lol");
   
-  let replies = ["What are the magic numbers on your parents credit card?", "On a scale of 1-10, how much of a weeb are you?", "Where did you hide the bodies?", "On a scale of `The best` to `Better than the best` how much of a great supreme is Kiwi?", "How do I hide multiple Orange coprses?", "That Malice kid is pretty trash, amight?", "Who's Grass?"];
+  let replies = ["What are the magic numbers on your parents credit card?", "On a scale of 1-10, how much of a weeb are you?", "Where did you hide the bodies?", "On a scale of `The best` to `Better than the best` how much of a great supreme is Kiwi?", "How do I hide multiple Orange coprses?", "That Malice kid is pretty trash, amight?", "Grass who?", "Which bot is best? (HINT: It starts with Scrotum and ends with Totem)"];
   let result = Math.floor((Math.random() * replies.length));
 
   //  let question = args.slice(1).join(" ");
