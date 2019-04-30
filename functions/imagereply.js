b9bc59fb36f60ae44e0362d164a671bf8bc74b69
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-04-30 13:36:23 +1200

    Update imagereply.js

--- functions/imagereply.js
+++ functions/imagereply.js
@@ -2,6 +2,12 @@ const fetch = require("node-fetch");
 const { RichEmbed } = require('discord.js');
 
 module.exports = {
+    /**
+     * Returns a random image from a random subreddit specified by the subredditNameArray.
+     * @param {snoowrap instance} r
+     * @param {Array[String]} subredditNameArray
+     * @return {String} URL of the image
+     */
     getSubredditImage: async function(r, subredditNameArray) {
         const subredditChoice = getRandomEntry(subredditNameArray);
 
@@ -24,6 +30,34 @@ module.exports = {
         return randomPost.data.url;
     },
 
+    /**
+     * Returns a random image from a random subreddit specified by the subredditNameArray, together with a post title and author
+     * @param {snoowrap instance} r
+     * @param {Array[String]} subredditNameArray
+     * @return {{title: title, author: author, url: imageUrl}}
+     */
+    getSubredditImagePost: async function(r, subredditNameArray) {
+        const subredditChoice = getRandomEntry(subredditNameArray);
+
+        // Try to fetch random submission first, not dependent on the hot page
+        const limit = 10;
+        var numberOfTries = 0;
+        do {
+            const post = await r.getSubreddit(subredditChoice).getRandomSubmission();
+            const allowed = post.post_hint === 'image' && !post.over_18;
+            if (allowed) return { title: post.title, author: post.author.name, url: post.url };
+            numberOfTries++;
+        } while (numberOfTries < limit);
+
+        // Random submission failed; fetch data from the hotpage
+        const data = await fetch(`https://www.reddit.com/r/${subredditChoice}/hot/.json?limit=100`).catch(console.error);
+        const json = await data.json();
+        const posts = json.data.children;
+        const allowedPosts = posts.filter(post => !post.data.over_18 && post.data.post_hint === 'image')
+        const randomPost = getRandomEntry(allowedPosts);
+        return { title: randomPost.data.title, author: randomPost.data.author, url: randomPost.data.url };
+    },
+
     getImageCatAPI: async function(url) {
         const data = await fetch(url).catch(console.error);
         const json = await data.json();
@@ -32,6 +66,14 @@ module.exports = {
 
     sendImageEmbed: async function(channel, url) {
         var embed = new RichEmbed()
+            .setImage(url)
+            .setColor(0xC9DDFF)
+        let msg = await channel.send({ embed });
+    },
+
+    sendImageEmbedWithAuthor: async function(channel, url, author) {
+        var embed = new RichEmbed()
+            .setTitle("From /u/" + author)
             .setImage(url)
             .setColor(0xC9DDFF)
         let msg = await channel.send({ embed });
