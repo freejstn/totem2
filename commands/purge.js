a36d0162e2969e213e0db65d8876912a0911b2e4
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-05-05 13:02:18 +1200

    fixed the if else statements

--- commands/purge.js
+++ commands/purge.js
@@ -1,17 +1,19 @@
 exports.run = async(client, message, args) => {
    if (args.length > 0) {
-     message.channel.send("Please define a number of messages to be yeeted");
-   }
-  else {
-  const messagecount = parseInt(args.join(' '));
+     const messagecount = parseInt(args.join(' '));
   message.channel.fetchMessages({
     limit: messagecount
   }).then(messages => message.channel.bulkDelete(messages));
   
   const msg = await message.channel.send("Done.");
   msg.delete(5000);
+   }
+  else {
+  message.channel.send("Please define a number of messages to be yeeted");
   }
 };
+
+
 
 exports.conf = {
   enabled: true,
