fff5ae1eadab81f87a28de5a327ba35854ced12e
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-05-05 13:01:04 +1200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,5 +6,5 @@ module.exports = async client => {
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 
   // Make the bot "play the game" which is the help command with default prefix.
-  client.user.setActivity("v2.1 | $help", {type: "PLAYING"});
+  client.user.setActivity("v2.2 | $help", {type: "PLAYING"});
 };
