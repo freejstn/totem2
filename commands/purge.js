99f087e103c059dab0b0e8b243c405bf6bb59378
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-05-06 16:05:46 +1200

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,12 +1,11 @@
 exports.run = async(client, message, args) => {
+   
    if (args.length > 0) {
      const messagecount = parseInt(args.join(' '));
-  message.channel.fetchMessages({
-    limit: messagecount
-  }).then(messages => message.channel.bulkDelete(messages));
+       message.channel.fetchMessages({
+      limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
   
-  const msg = await message.channel.send("Done.");
-  msg.delete(5000);
+
    }
   else {
   message.channel.send("Please define a number of messages to be yeeted");
