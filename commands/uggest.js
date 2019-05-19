750b9339b13f072cafe7755930affd6014a2208a
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-05-19 17:23:36 +1200

    Update uggest.js

--- commands/uggest.js
+++ commands/uggest.js
@@ -6,7 +6,7 @@ var reportChannelID = 486578098217811978;
 var suggestChannelID = 529547120429236225;
 var suggestionCount = 0; //this will reset whenever the bot goes down, but it's cool so lmao
 
-client.channels.get("529547120429236225").send("Suggestion Number " + suggestionCount + ":" + arguments);
+client.channels.get("529547120429236225").send("Suggestion Number " + suggestionCount + ":" + args);
     message.delete();
     message.channel.author.send("Thank you for your Anonymous Suggestion!");
   
