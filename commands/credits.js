bd873fe132aa0fd5bdf25beacc9b0482b54fc68d
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-05-22 20:50:05 +1200

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -4,7 +4,7 @@ const { RichEmbed } = require('discord.js');
 exports.run = async (client, message, args, level) => {
         //message.delete(1000);
         const footerimage = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/185/green-heart_1f49a.png"
-        const authorImage = "//s.imgur.com/min/embed.js"
+        const authorImage = "https://i.ibb.co/WVjcXTd/scrotum-totem.jpg"
    
         
         
