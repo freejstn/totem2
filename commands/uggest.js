1cbed432a93854f8fd472069cbd8c76077c69cd5
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-05-23 23:26:41 +1200

    Update uggest.js

--- commands/uggest.js
+++ commands/uggest.js
@@ -1,17 +1,20 @@
 const { RichEmbed } = require('discord.js');
 
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-
+        
+        //delete the users message after 1000ms
         message.delete(1000);
+        //set the suggestion var to the argument - join makes it output nicely
         const suggestion = args.join(' ')
+        //get the channel id for the suggest channel and save it as a var
         const suggestchannel = client.channels.get("581061069250887690")
+        //save the authors username as "name"
         const name = message.author.username + ' suggested'
         
         if (!suggestchannel) return message.reply('I cannot find the suggestions channel');
         //if (position.length < 1) return message.reply('You must supply a position for the person.');
         if (suggestion < 1) return message.reply('No suggestion found').catch(console.error);
        
-        //const embed = new Discord.RichEmbed()
         var embed = new RichEmbed()
             .setColor('#68AD36')
             //.setTitle("Suggested")
@@ -22,18 +25,14 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 
             //embed.addField(suggestion)
             
-
-        //let msg = await message.channel.send({ embed });
-        //return client.channels.get(suggestchannel.id).send({embed});
         let msg = client.channels.get(suggestchannel.id).send({embed}).then(sentEmbed => {
         sentEmbed.react('👍').then(() => sentEmbed.react('👎').then(() => sentEmbed.react('🤷')));
+                
+        message.channel.author.send("Thank you for your Suggestion!");
 });
 };        
 
-
-    //client.channels.get("529547120429236225").send("Suggestion Number " + suggestionCount + ":" + args);
-    //message.delete(1000);
-    //message.channel.author.send("Thank you for your Anonymous Suggestion!");
+    
   
 
 
