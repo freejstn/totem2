6adad5916a8c1c42f7b434aa3fa21db7bf28ac7f
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-06-02 11:23:07 +1200

    Create elect.js

---
+++ commands/elect.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  msg.react('584174141737926666');
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "Administrator"
+};
+
+exports.help = {
+  name: "elect",
+  category: "Important",
+  description: "Election command to give flag react",
+  usage: "elect"
+};
