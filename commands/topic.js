b001487e66974cca1c4f9ee527acfe982a137b49
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-06-02 18:08:25 +1200

    Added dialga's topic

--- commands/topic.js
+++ commands/topic.js
@@ -1,7 +1,23 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
   //const msg = await message.channel.send("coming soon lol");
   
-  let replies = ["What are the magic numbers on your parents credit card?", "On a scale of 1-10, how much of a weeb are you?", "Where did you hide the bodies?", "On a scale of `The best` to `Better than the best` how much of a great supreme is Kiwi?", "How do I hide multiple Orange coprses?", "That Malice kid is pretty trash, amight?", "Grass who?", "Which bot is best? (HINT: It starts with Scrotum and ends with Totem)", "How is the Scrotum Totem so damn amazing?", "Who is the most attractive Green","If you had to choose one person to exile from Green, not due to personal opinion but solely because based on their crimes against humanity, who would it be?", "Is there anybody in the world who can challenge beet for the title of biggest weeb?", "Why did papa Heli leave us :(", "Which color would create the most chaos if it was suddenly merged with Green?", "If you could plant a spy in any colours government, who would you choose and which colour?", "What is the best psychological test to determine how much of a weeb someone is?"];
+  let replies = ["What are the magic numbers on your parents credit card?", 
+                 "On a scale of 1-10, how much of a weeb are you?", 
+                 "Where did you hide the bodies?", 
+                 "On a scale of `The best` to `Better than the best` how much of a great supreme is Kiwi?", 
+                 "How do I hide multiple Orange coprses?", 
+                 "That Malice kid is pretty trash, amight?", 
+                 "Grass who?", 
+                 "Which bot is best? (HINT: It starts with Scrotum and ends with Totem)", 
+                 "How is the Scrotum Totem so damn amazing?", 
+                 "Who is the most attractive Green",
+                 "If you had to choose one person to exile from Green, not due to personal opinion but solely because based on their crimes against humanity, who would it be?", 
+                 "Is there anybody in the world who can challenge beet for the title of biggest weeb?", 
+                 "Why did papa Heli leave us :(", 
+                 "Which color would create the most chaos if it was suddenly merged with Green?", 
+                 "If you could plant a spy in any colours government, who would you choose and which colour?", 
+                 "What is the best psychological test to determine how much of a weeb someone is?",
+                "Is my mother ever going to return,or has she abandoned me for good"];
   let result = Math.floor((Math.random() * replies.length));
 
   //  let question = args.slice(1).join(" ");
