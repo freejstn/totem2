08cb9421183cef2bfa8bbc63ad706321ab451630
KiwisGithub <41028518+KiwisGithub@users.noreply.github.com>
2019-06-02 12:05:40 +1200

    Update napme.js

--- commands/napme.js
+++ commands/napme.js
@@ -1,12 +1,12 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
- const msg = await message.channel.send("***SNAP***").then(() => msg.react('584174001547640852').then(() => msg.react('584174001199251506')));
+ 
   
  let replies = ["You have been Snapped.", "You have been Spared."];
  let result = Math.floor((Math.random() * replies.length));
 
+ const msg = await message.channel.send("***SNAP***").then(() => msg.react('584174001547640852').then(() => msg.react('584174001199251506')));
  msg.edit(replies[result]);
   //const msg = await message.channel.send(replies[result]).then(() => msg.react('584174001128210442');
-
 
 };
 
